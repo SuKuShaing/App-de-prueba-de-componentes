651cb2eba96eca2420603f47785d11149611ab29
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-08-26 16:17:42 -0400

    carga infinita con FlatList

--- app/infinite-scroll/index.tsx
+++ app/infinite-scroll/index.tsx
@@ -1,10 +1,54 @@
-import { View, Text } from 'react-native';
+import ThemedView from "@/presentation/shared/ThemedView";
+import { useState } from "react";
+import { FlatList, Image } from "react-native";
 
 const InfiniteScrollScreen = () => {
-  return (
-    <View>
-      <Text>InfiniteScrollScreen</Text>
-    </View>
-  );
+	const [numbers, setNumbers] = useState([0, 1, 2, 3, 4, 5]);
+
+	const loadMore = () => {
+		const newArray = Array.from(
+			{ length: 5 },
+			(_, index) => numbers.length + index + 1
+		);
+
+		console.log("Loading more...");
+		setTimeout(() => {
+			setNumbers([...numbers, ...newArray]);
+		}, 2000);
+	};
+
+	return (
+		<ThemedView>
+			<FlatList
+				// El FlatList renderiza los elementos bajo demanda, solo que está en pantalla se renderiza y lo demás no, solo está en memoria.
+				data={numbers}
+				// data es un array de items a iterar
+				renderItem={({ item }) => <ListItem number={item} />}
+				// renderItem es el elemento que se renderiza en pantalla
+				onEndReached={loadMore}
+				// onEndReached es el evento que se ejecuta cuando el usuario llega al final de la lista
+				onEndReachedThreshold={0.4}
+				// onEndReachedThreshold es el umbral de la lista para que se ejecute el evento onEndReached
+			/>
+		</ThemedView>
+	);
 };
 export default InfiniteScrollScreen;
+
+
+// Componente a Renderizar
+interface ListItemProps {
+	number: number;
+}
+
+const ListItem = ({ number }: ListItemProps) => {
+	return (
+		<Image
+			source={{ uri: `https://picsum.photos/id/${number}/500/400` }}
+			style={{
+				height: 400,
+				width: "100%",
+			}}
+		/>
+	);
+};
