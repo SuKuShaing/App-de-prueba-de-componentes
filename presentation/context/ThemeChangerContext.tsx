0be00607a4fee59d9889042f5381a9e8a2089e66
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-08-29 13:39:32 -0400

    se instala un Store Data, el Async Storage para almacenar y recuperar el theme seleccionado por el usuario

--- presentation/context/ThemeChangerContext.tsx
+++ presentation/context/ThemeChangerContext.tsx
@@ -1,11 +1,12 @@
 // import { useColorScheme } from "@/hooks/useColorScheme.web";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import {
     DarkTheme,
     DefaultTheme,
     ThemeProvider,
 } from "@react-navigation/native";
 import { useColorScheme } from "nativewind";
-import { createContext, PropsWithChildren, useContext, useState } from "react";
+import { createContext, PropsWithChildren, useContext, useEffect, useState } from "react";
 
 /*
 el desarrollo de esto se explica en la clase 150 de React Native con Expo por Fernando Herrera
@@ -41,6 +42,18 @@ export const ThemeChangerProvider = ({ children }: PropsWithChildren) => {
 			? "dark"
 			: "light";
 
+	useEffect(() => {
+
+        AsyncStorage.getItem('selected-theme').then((theme) => {
+            if (!theme) return;
+
+            setIsDarkMode(theme === 'dark');
+            setIsSystemThemeEnabled(theme === 'system');
+            setColorScheme(theme as 'light' | 'dark' | 'system'); // as es para decirle a typescript que el theme es de esos tipos, debiésemos evaluar previamente pero lo hacemos así para no calentarnos la cabeza
+        })
+
+	}, []);
+
 	return (
 		<ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
 			<ThemeChangerContext.Provider
@@ -48,17 +61,24 @@ export const ThemeChangerProvider = ({ children }: PropsWithChildren) => {
 					currentTheme: currentTheme ?? "light",
 					isSystemTheme: isSystemThemeEnabled,
 					toggleTheme: async () => {
-                        setIsDarkMode(!isDarkMode);
-                        setColorScheme(isDarkMode ? "light" : "dark");
-                        setIsSystemThemeEnabled(false);
+						setIsDarkMode(!isDarkMode);
+						setColorScheme(isDarkMode ? "light" : "dark");
+						setIsSystemThemeEnabled(false);
 
-                        // To Do: Guardar el tema en el storage
-                    },
+						// Guardar el theme en el storage
+						await AsyncStorage.setItem(
+							"selected-theme",
+							isDarkMode ? "light" : "dark"
+						);
+					},
 
 					setSystemTheme: async () => {
-                        setIsSystemThemeEnabled(true);
-                        setColorScheme('system');
-                    },
+						setIsSystemThemeEnabled(true);
+						setColorScheme("system");
+
+						// Guardar el theme en el storage
+						await AsyncStorage.setItem("selected-theme", "system");
+					},
 				}}
 			>
 				{children}
